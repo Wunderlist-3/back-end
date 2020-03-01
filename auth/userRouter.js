48d146d672c95c6a122bb0083c4caf56319f92f7
Alexander Sierra <alexandercsierra@gmail.com>
2020-03-01 01:20:47 -0500

    database created

--- auth/userRouter.js
+++ auth/userRouter.js
@@ -1,7 +1,15 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const Users = require('./userModel');
+const {jwtSecret} = require('./secrets');
+const jwt = require('jsonwebtoken');
 
-router.get('/', (req, res)=>{
-    res.status(200).json({message:'welcome to the userrouter'})
+
+router.post('/register', (req, res)=>{
+    let newUser = req.body;
+    const hash = bcrypt.hashSync(newUser.password, 12);
+    newUser.password = hash;
+    
 })
 
 module.exports=router;
